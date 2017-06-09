5f3472b158a377b0999af11645393e2bb7c96dac
Justin LeFurjah <lefurjah@gmail.com>
2017-06-09 12:02:00 -0500

    style changes

--- vivial_connect_example.js
+++ vivial_connect_example.js
@@ -1,82 +1,90 @@
-// Install dependencies (request, moment, crypto-js, url)
-// Fill in the empty constants (ln 15 - ln 19)
-// Execute the script with the following command: node vivial_connect_example.js
-// This will send a message to the "TO_NUMBER" you provided
+/* 
+ Install dependencies (request, moment, crypto-js, url)
+ Fill in the empty constants (ln 15 - ln 19)
+ Execute the script with the following command: node vivial_connect_example.js
+ This will send a message to the 'to_number' you provided
+*/
 
-"use strict"
+'use strict';
 
-const request = require('request')
-const moment = require('moment')
-const cryptoJS = require('crypto-js')
-const URL = require('url').URL;
+const request = require('request');
+const moment = require('moment');
+const cryptoJS = require('crypto-js');
+const url_lib = require('url').URL;
 
-const BASE_URL = 'https://api.vivialconnect.net/api/v1.0'
-const CANONICALIZED_HEADER_NAMES = "accept;date;host"
-const API_KEY = ''
-const API_SECRET = ''
-const ACCOUNT_ID = ''
-const TO_NUMBER = ''
-const FROM_NUMBER = ''
+const base_url = 'https://api.vivialconnect.net/api/v1.0';
+const canonicalized_header_names = 'accept;date;host';
+const api_key = '';
+const api_secret = '';
+const account_id = '';
+const to_number = '';
+const from_number = '';
 
- function signRequest(method, url, data){
+function signRequest(method, url, data){
 
-  let now = moment.utc()
-  let requestTimestamp = now.format('YYYYMMDD[T]HHmmss[Z]')
-  let dateForHeader = now.format('ddd, DD MMM YYYY HH:mm:ss [GMT]')
-  let SHA256data = cryptoJS.SHA256(data)
-  let myURL = new URL(url);
-  let path = myURL.pathname
-  let sortedParams = ''
-  let canonicalizedHeaders = "accept:application/json" + '\n' + `date:${dateForHeader}` + '\n' +"host:api.vivialconnect.net"
+    var now = moment.utc();
+    var requestTimestamp = now.format('YYYYMMDD[T]HHmmss[Z]');
+    var dateForHeader = now.format('ddd, DD MMM YYYY HH:mm:ss [GMT]');
+    var SHA256data = cryptoJS.SHA256(data);
+    var myURL = new url_lib(url);
+    var path = myURL.pathname;
+    var sortedParams = '';
+    var canonicalizedHeaders = 'accept:application/json' + '\n' + `date:${dateForHeader}` + '\n' +'host:api.vivialconnect.net';
 
-  let canonicalRequest = method + '\n' + 
-                         requestTimestamp + "\n" +
+    var canonicalRequest = method + '\n' + 
+                         requestTimestamp + '\n' +
                          path + '\n' +
                          sortedParams + '\n' +
                          canonicalizedHeaders + '\n' +
-                         CANONICALIZED_HEADER_NAMES + '\n' +
-                         SHA256data
+                         canonicalized_header_names + '\n' +
+                         SHA256data;
 
 
-  let signature = cryptoJS.HmacSHA256(canonicalRequest, API_SECRET)
+    var signature = cryptoJS.HmacSHA256(canonicalRequest, api_secret);
 
-  let signatureData = {
-    dateForHeader: dateForHeader,
-    requestTimestamp: requestTimestamp,
-    signature: signature
-  }
+    var signatureData = {
+        'dateForHeader': dateForHeader,
+        'requestTimestamp': requestTimestamp,
+        'signature': signature
+    };
 
-  return signatureData
+    return signatureData
 }
 
-  function clientRequestor(method, url, data) {
-    let signatureData = signRequest(method, url, data)
-      let options = {
-        url: url,
-        method: method,
-        headers: {
-          'Accept': 'application/json',
-          'Date': signatureData.dateForHeader,
-          'Host': 'api.vivialconnect.net',
-          'Content-Type': 'application/json',
-          'X-Auth-Date': signatureData.requestTimestamp,
-          'X-Auth-SignedHeaders': 'accept;date;host',
-          Authorization: 'HMAC ' + API_KEY + ':' + signatureData.signature
+function makeRequest(method, url, data) {
+    var signatureData = signRequest(method, url, data);
+    var options = {
+        'url': url,
+        'method': method,
+        'headers': {
+            'Accept': 'application/json',
+            'Date': signatureData.dateForHeader,
+            'Host': 'api.vivialconnect.net',
+            'Content-Type': 'application/json',
+            'X-Auth-Date': signatureData.requestTimestamp,
+            'X-Auth-SignedHeaders': 'accept;date;host',
+            Authorization: 'HMAC ' + api_key + ':' + signatureData.signature
         },
         body: data
-      }
+    };
 
-      request(options, function (error, response, body) {
-          console.log('error:', error)
-          console.log('statusCode:', response && response.statusCode)
-          console.log('body:', body)
-      })
-  }
-
+    request(options, function (error, response, body) {
+        console.log('error:', error)
+        console.log('statusCode:', response && response.statusCode)
+        console.log('body:', body)
+    });
+}
 
 (function(){
-  let data = { 'message': {'to_number': TO_NUMBER, 'from_number': FROM_NUMBER, 'body': 'Hello from Vivial Connect Node.js example'}}
-  data = JSON.stringify(data)
-  let url = `${BASE_URL}/accounts/${ACCOUNT_ID}/messages.json`
-  clientRequestor("POST", url , data)
+    var data = { 
+        'message': {
+            'to_number': to_number, 
+            'from_number': from_number, 
+            'body': 'Hello from the Vivial Connect Node.js example'
+        }
+    };
+
+    data = JSON.stringify(data)
+    var url = `${base_url}/accounts/${account_id}/messages.json`
+    makeRequest('POST', url , data)
 })();
